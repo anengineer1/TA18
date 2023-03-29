22f9f1f574325dbc3e16ad7f66aa76942b670176
Palmira Romia <kguinera@hotmail.com>
2023-03-30 00:31:37 +0200

    Creación de package y clase para ejercicio9

---
+++ src/ejercicio_9/Ejercicio_9_App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ejercicio_9;
+
+/**
+ * @author Palmira
+ *
+ */
+public class Ejercicio_9_App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

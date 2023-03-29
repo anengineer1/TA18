a704774fe869580f207bc9aca633485a568e2a6a
Palmira Romia <kguinera@hotmail.com>
2023-03-29 23:52:40 +0200

    Ejercicio 3 completo

--- src/ejercicio_3/Ejercicio_3_App.java
+++ src/ejercicio_3/Ejercicio_3_App.java
@@ -35,9 +35,9 @@ public class Ejercicio_3_App {
 		MySqlConnectionHandler.createTable(dbase_name, tablaAlmacenes, queryAlmacenes, con_handler);
 		// creamos las variables que usaremos para crear la tabla cajas
 		String tablaCajas = "cajas";
-		String queryCajas = "numReferencia CHAR(5) PRIMARY KEY, " + "contenido VARCHAR(100), " + "almacen_id INT, "
-				+ "CONSTRAINT almacen_id FOREIGN KEY(almacen_id)" + "" + "REFERENCES almacenes (codigo) "
-				+ "ON DELETE CASCADE ON UPDATE CASCADE";
+		String queryCajas = "numReferencia CHAR(5) PRIMARY KEY, " + "contenido VARCHAR(100), " + "valor INT, "
+				+ "almacen_id INT, " + "CONSTRAINT almacen_id FOREIGN KEY(almacen_id)" + ""
+				+ "REFERENCES almacenes (codigo) " + "ON DELETE CASCADE ON UPDATE CASCADE";
 		// Creamos la tabla Cajas
 		MySqlConnectionHandler.createTable(dbase_name, tablaCajas, queryCajas, con_handler);
 
@@ -48,11 +48,33 @@ public class Ejercicio_3_App {
 		datos_Almacenes[2] = "3, 'Nueva York', 10";
 		datos_Almacenes[3] = "4, 'Edimburgo', 4";
 		datos_Almacenes[4] = "5, 'Paris', 6";
-		
-		for(int i = 0; i<datos_Almacenes.length; i++) {
+
+		for (int i = 0; i < datos_Almacenes.length; i++) {
 			MySqlConnectionHandler.insertData(dbase_name, tablaAlmacenes, datos_Almacenes[i], con_handler);
 		}
 
+		// Inserción en la tabla Cajas
+		String[] datos_Cajas = new String[5];
+		datos_Cajas[0] = "'1D5E', 'Patatas', 10, 3";
+		datos_Cajas[1] = "'1R7T', 'Kiwis', 5, 2";
+		datos_Cajas[2] = "'2V6R', 'Manzanas', 20, 1";
+		datos_Cajas[3] = "'3M4N', 'Melocotones', 15, 5";
+		datos_Cajas[4] = "'8Z5L', 'Piñas', 25, 4";
+
+		for (int i = 0; i < datos_Cajas.length; i++) {
+			MySqlConnectionHandler.insertData(dbase_name, tablaCajas, datos_Cajas[i], con_handler);
+		}
+
+		// Imprimimos la tabla Almacenes
+		System.out.println("Tabla " + tablaAlmacenes);
+		MySqlConnectionHandler.printValues(dbase_name, tablaAlmacenes, con_handler);
+
+		// Imprimimos la tabla Cajas
+		System.out.println("Tabla " + tablaCajas);
+		MySqlConnectionHandler.printValues(dbase_name, tablaCajas, con_handler);
+
+		// Cerrar conexion
+		MySqlConnectionHandler.closeConnection(con_handler);
 	}
 
 }
